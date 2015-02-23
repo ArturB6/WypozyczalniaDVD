1f1a0733098387f97d3af44ad8e87f26303555cd
ArturB6 <arturb661@gmail.com>
2015-02-23 21:01:41 +0100

    Create orderNoLoginInfoDiv.php

---
+++ orderNoLoginInfoDiv.php
@@ -0,0 +1,7 @@
+<div id="orderNoLoginInfoDiv">
+<p>Aby złozyc zamowienie, konieczne jest
+<a href="index.php?action=showLoginForm">zalogowanie</a>.</p>
+<p>Jesli nie masz jeszcze konta w naszym serwisie,
+<a href='index.php?action=showRegistrationForm'>załoz je teraz</a>.</p>
+<p><a href='index.php?action=showBasket'>Wroc do koszyka</a>
+</div>
