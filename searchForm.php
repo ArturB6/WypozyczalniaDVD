e90512faba1bd079b957fd00fd18236b1557e942
maroooo <slosarczyk.marek@gmail.com>
2015-02-23 22:25:48 +0100

    Create searchForm.php

---
+++ searchForm.php
@@ -0,0 +1,10 @@
+<div id="searchFormDiv">
+<form action='index.php'
+      method='GET'
+>
+<input type="hidden" name="action" value="searchFilm">
+Autor:<input size='25' type='text' name='autor' value='' />
+Tytuł:<input size='25' type='text' name='tytul' value='' />
+<input type='submit' value='Szukaj'>
+</form>
+</div>
